6d7f5364d7363fef3ed375d02107dccb920ffcd8
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-14 12:22:02 -0700

    callCityAPI now retrieves and console logs the lat/long for the city searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,16 +26,41 @@ retrieveHistory();
 var userCitySearch = function() {
     citySearchString = cityInputEl[0].value;
     if(citySearchString) {
-        // callCityAPI(citySearchString);
+         callCityAPI(citySearchString);
         searchHistory.push(citySearchString);
          localStorage.setItem('citySearchHistory', JSON.stringify(searchHistory));
-        populateHistory();
+     //   populateHistory();
         cityInputEl[0].value = "";
     }
     else {
         alert("Please enter a city.")
     }
 }
+
+// function to gather city longitude and latitude to use in the openweather api
+var callCityAPI = function(city) {
+    cityAPISearch = `https://api.openweathermap.org/geo/1.0/direct?q=
+                    ${city}&limit=1&appid=94e32ddc97880c45b19a69dfc85aec8d`;
+    fetch(cityAPISearch)
+    .then(response => response.json())
+    .then(function(data) {
+        cityLat = data[0].lat;
+        cityLong = data[0].long;
+        JSON.stringify(cityLat, cityLong);
+        console.log(cityLat, cityLong);
+      //  callWeatherAPI(cityLat,cityLong);
+    })
+};
+
+
+
+
+
+
+
+
+
+
 
 
 
