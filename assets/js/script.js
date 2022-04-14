bf55e1c9949fb1b6c02d47837ed2aef3df1aede4
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-14 11:28:15 -0700

    var and const  declarations in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// VARIABLE DECLARATION 
+var forecastWeather;
+var cityLat;
+var cityLong;
+var cityAPISearch = [];
+var citySearchString;
+var searchHistory =[];
+var dateFormatCurrent;
+var dateFormatForecast = [];
+var dtForeCast;
+
+
+const searchButtonEl = $('#search');
+const cityInputEl = $('#city-search');
+
