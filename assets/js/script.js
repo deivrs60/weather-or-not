220d25499c5a6a2219109ffa206628d8dae63f9d
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-14 11:43:47 -0700

    partial search button function without localStorage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,20 @@ var dtForeCast;
 const searchButtonEl = $('#search');
 const cityInputEl = $('#city-search');
 
+
+// search button functionality 
+var userCitySearch = function() {
+    citySearchString = cityInputEl[0].value;
+    if(citySearchString) {
+        callCityAPI(citySearchString);
+        searchHistory.push(citySearchString);
+// localStorage.setItem('citySearchHistory', 
+//  JSON.stringify(searchHistory));
+        populateHistory();
+        cityInputEl[0].value = "";
+    }
+    else {
+        alert("Please enter a city.")
+    }
+}
+
