09a1d1bcc695a3b7baebd1726faff7bb61e3e64c
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-14 15:21:29 -0700

    updated API keys in attempt to make the app work, keeps returning 401 errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,18 +40,55 @@ var userCitySearch = function() {
 // function to gather city longitude and latitude to use in the openweather api
 var callCityAPI = function(city) {
     cityAPISearch = `https://api.openweathermap.org/geo/1.0/direct?q=
-                    ${city}&limit=1&appid=94e32ddc97880c45b19a69dfc85aec8d`;
+                    ${city}&limit=1&appid=a0b786fdc717dfea3eb829a24bf465f2`;
     fetch(cityAPISearch)
     .then(response => response.json())
     .then(function(data) {
         cityLat = data[0].lat;
         cityLong = data[0].long;
-        JSON.stringify(cityLat, cityLong);
-        console.log(cityLat, cityLong);
-      //  callWeatherAPI(cityLat,cityLong);
+        callWeatherAPI(cityLat,cityLong);
     })
 };
 
+
+// function to get the searched city's weather conditions based on the LAT and LONG gathered from the geocoding api
+var callWeatherAPI = function(cityLat, cityLong){
+    var currentWeatherAPICall = `https://api.openweathermap.org/data/2.5/onecall?
+                                lat=${cityLat}&
+                                lon=${cityLong}&
+                                units=${'imperial'}&
+                                exclude=${'alerts,hourly,minutely'}&
+                                appid=${'a0b786fdc717dfea3eb829a24bf465f2'}`;
+    fetch(currentWeatherAPICall)
+    .then(response => response.json())
+    .then(function(data){
+        let currentWeather = {
+            'temp': data.current.temp,
+            'wind': data.current.wind_speed,
+            'humidity': data.current.humidity,
+            uvi: data.current.uvi
+        };
+    console.log(currentWeather)
+
+    var dtCurrent = data.current.dt;
+    dtForecast = data.daily;
+    var millsecondsCurrent = dtCurrent * 1000;
+    var dateObjectCurrent = new Date(millsecondsCurrent);
+    dateFormatCurrent = dateObjectCurrent.toLocaleDateString('en-US');
+    renderCurrentWeatherContent(currentWeather, data);
+
+    for (var i=1; i<6; i++) {
+        var millsecondsForecast = dtForecast[i].dt * 1000;
+        var dateObjectForecast = new Date(millsecondsForecast);
+        currentDate = dateObjectForecast.toLocaleDateString('en-US');
+        dateFormatForecast.push(currentDate);
+    }
+    renderForecastContent(data, dateFormatForecast);
+
+    });
+}
+
+// render the current weather conditions to content
 
 
 
@@ -65,4 +102,4 @@ var callCityAPI = function(city) {
 
 
 // SEARCH button click functionality
-searchButtonEl.on('click', userCitySearch)
+searchButtonEl.on('click', userCitySearch);
